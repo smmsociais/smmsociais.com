08ddabe8c36216bee6fecbe28c666c5c409cc633
SMM <contato@ganhesocial.com>
2025-12-09 14:06:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import apiRoutes from "./api/handler.js";
-import twoFARoutes from "./routes/twofa.js";
+import twoFARoutes from ".api/routes/twofa.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
